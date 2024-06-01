475390bcb23acd81a3b40cad7050e4dd45b00686
Snapemagical <snapemagical@snapemagical.com>
2024-06-01 23:03:14 +0530

    Committing fetch call.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import styles from "./page.module.css";
-// import Profile from './test';
+import Profile from './test';
 
-// const getData = async () => {
-//   const data = await fetch('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {cache: 'no-cache'});
-//   if (!data.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch data')
-//   }
+const getData = async () => {
+  const data = await fetch('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {cache: 'no-cache'});
+  if (!data.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
  
-//   return data.json()
-// }
+  return data.json()
+}
 
 
 export default async  function Home() {
-  // let data = await getData();
-
+  let data = await getData();
+  
   return (
     <main className={styles.main}>
       <h1>LTP calulator</h1>
-      {/* <Profile post={data}/> */}
+      <Profile post={data}/>
     </main>
   );
 }
