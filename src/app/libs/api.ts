fea6b5ab3980d8430de003805b58ad49bb6921ce
Snapemagical <snapemagical@snapemagical.com>
2024-06-03 23:59:02 +0530

    Committing new try.

--- src/app/libs/api.ts
+++ src/app/libs/api.ts
@@ -1,10 +1,11 @@
 export async function getData() {
   // const data = await fetch('http://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {cache: 'no-cache'});
-  // if (!data.ok) {
-  //   // This will activate the closest `error.js` Error Boundary
-  //   throw new Error('Failed to fetch data')
-  // }
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos', {cache: 'no-cache'});
+  if (!data.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
  
-  return {};
+  return data.json();
  
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Profile from './test';
 import { getData } from './libs/api'
+import { useEffect } from "react";
 
 // const getData = async () => {
 //   const data = await fetch('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {cache: 'no-cache'});
@@ -14,11 +15,21 @@ import { getData } from './libs/api'
 
 
 export default async  function Home() {
+  const getData = async () => {
+    const data = await fetch('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {cache: 'no-cache'});
+  if (!data.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+ 
+  return data.json()
+}
   let data = await getData();
   
   return (
     <main className={styles.main}>
       <h1>LTP calulator</h1>
+      {/* {{data}} */}
       {/* <Profile post={data}/> */}
     </main>
   );
